fix scaling of epsilon and add linesearch strategy for gradient descent step

extra_py.py
@@ -1,7 +1,7 @@
 import numpy as np
 import w2extrapolation
 
-def extra(nu0,nu1,f0,Z0,t,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,verb=0):
+def extra(nu0,nu1,f0,Z0,t,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=None,bb=True,verb=0):
 
     # input:
     # nu0 -> initial measure; tuple containing two numpy arrays, X the particles' positions of size (n,d) and mu the weights of size n
@@ -15,6 +15,7 @@ def extra(nu0,nu1,f0,Z0,t,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,verb=0):
     # itmax -> maximum number of iterations for the SISTA algorithm
     # tol -> tolerance for the error
     # tau -> stepsize for the gradient descent step in the SISTA algorithm
+    # bb -> logical; True for adapative Barzilai–Borwein stepsize or False for constant stepsize tau
     # verb -> verbosity level: 0 no output, 1 print iterations' details
 
     # output: 
@@ -33,7 +34,9 @@ def extra(nu0,nu1,f0,Z0,t,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,verb=0):
     m=b.shape[0]
     d=X.shape[1]
 
-    f, g, Z, P, niter, errP, errG, obj = w2extrapolation.extrapolation.extra_fortran(n,m,d,a,b,X,Y,f0,Z0,t,eps,deb,itmax,tol,tau,verb) 
+    if tau==None: tau= 1/( 1.0/t + np.max(b)/(t-1.0) ) # inverse of the approximated Lipschitz constant
+
+    f, g, Z, P, niter, errP, errG, obj = w2extrapolation.extrapolation.extra_fortran(n,m,d,a,b,X,Y,f0,Z0,t,eps,deb,itmax,tol,tau,bb,verb) 
     mu=(Z,b)
     dt=1/(1-t)
     Xb=dt*Z+(1-dt)*Y
@@ -43,7 +46,7 @@ def extra(nu0,nu1,f0,Z0,t,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,verb=0):
 
     return f, g, mu, P, nu0bar, niter, errP, errG, obj
 
-def extra_Nto1(num,nup,f0,Z0,t,lm,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,verb=0):
+def extra_Nto1(num,nup,f0,Z0,t,lm,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=None,bb=True,verb=0):
 
     # input:
     # num -> initial measures; tuple containing the N measures in the form of tuples (X,a), 
@@ -59,6 +62,7 @@ def extra_Nto1(num,nup,f0,Z0,t,lm,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,
     # itmax -> maximum number of iterations for the SISTA algorithm
     # tol -> tolerance for the error
     # tau -> stepsize for the gradient descent step in the SISTA algorithm
+    # bb -> logical; True for adapative Barzilai–Borwein stepsize or False for constant stepsize tau
     # verb -> verbosity level: 0 no output, 1 print iterations' details
 
     # output: 
@@ -79,6 +83,8 @@ def extra_Nto1(num,nup,f0,Z0,t,lm,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,
     Y, b=nup[0], nup[1]
     m=b.shape[0]
     d=Y.shape[1]
+
+    if tau==None: tau= 1/( 1.0/t + np.max(b)/(t-1.0) ) # inverse of the approximated Lipschitz constant
     
     a_all=np.zeros((nb,Nm))
     f0all=np.zeros((nb,Nm))
@@ -90,7 +96,7 @@ def extra_Nto1(num,nup,f0,Z0,t,lm,eps=1e-3,deb=False,itmax=10000,tol=5e-4,tau=1,
     n=np.array(n)
     lm=np.array(lm)
 
-    fall, g, Z, Pall, niter, errP, errG, obj = w2extrapolation.extrapolation.extra_nto1_fortran(Nm,nb,n,m,d,a_all,b,Xall,Y,f0all,Z0,t,lm,eps,deb,itmax,tol,tau,verb)
+    fall, g, Z, Pall, niter, errP, errG, obj = w2extrapolation.extrapolation.extra_nto1_fortran(Nm,nb,n,m,d,a_all,b,Xall,Y,f0all,Z0,t,lm,eps,deb,itmax,tol,tau,bb,verb)
     f=tuple( [fall[0:n[k],k] for k in range(Nm)] )
     P=tuple( [Pall[0:n[k],:,k] for k in range(Nm)] )
     mu=(Z,b)

test_extra_Nto1.py
@@ -43,7 +43,7 @@ for i in range(1,Nt+1):
     if i==1:
         f0=tuple([np.ones(num[k][0].shape[0]) for k in range(Nm)])
         Z0=nup[0]
-    f, g, mu, P, numbar, niter, errP, errG, obj =extra.extra_Nto1(num,nup,f0,Z0,t,lm,eps,False,itmax,tol,tau,1)
+    f, g, mu, P, numbar, niter, errP, errG, obj =extra.extra_Nto1(num,nup,f0,Z0,t,lm,eps=eps,deb=True,itmax=itmax,tol=tol,verb=0,bb=True)
 
     print('iterations:',niter,'err.marg.:',errP[niter-1],'err.grad.:',errG[niter-1])
     if max(errP[niter-1],errG[niter-1])>tol:

test_extra_n.py
@@ -9,7 +9,6 @@ dt=(T-1)/Nt
 d=2 # dimension
 
 eps=1e-3 # temperature value
-tau=5 # stepsize
 itmax=50000 # maximum number of iterations
 tol=5e-4 # stop tolerance
 
@@ -50,7 +49,7 @@ for ind in couples:
         if i==1:
             f0=np.ones(np.size(X,0))
             Z0=Y
-        f, g, mu, P, nu0bar, niter, errP, errG, obj =extra.extra((X,a),(Y,b),f0,Z0,t,eps,False,itmax,tol,tau,0)
+        f, g, mu, P, nu0bar, niter, errP, errG, obj =extra.extra((X,a),(Y,b),f0,Z0,t,eps=eps,deb=True,itmax=itmax,tol=tol,verb=0,bb=True)
 
         print(ind[0]+'2'+ind[1],'iterations:',niter,'err.marg.:',errP[niter-1],'err.grad.:',errG[niter-1])
         if max(errP[niter-1],errG[niter-1])>tol: