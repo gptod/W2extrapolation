4b5111530452fa1ad5ae0e37b95b22c71043de9e
Andrea Natale <nichel27@gmail.com>
2026-04-02 00:30:04 +0200

    Change wp parameter type from sp to dp

--- global.f90
+++ global.f90
@@ -3,6 +3,6 @@ module para
     implicit none
     integer, parameter :: sp=kind(1.0e0)
     integer, parameter :: dp=kind(1.0d0)
-    integer, parameter :: wp=sp
+    integer, parameter :: wp=dp
 
 end module para
